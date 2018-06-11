1af73cefaa17839b45f1da3135a1f21689b8f300
Nieg <xp_fei@163.com>
2018-06-12 00:17:52 +0800

    更新 gulp-minify-css->gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var minifycss = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
@@ -8,9 +8,10 @@ var prettyData = require('gulp-pretty-data');
 // 压缩css文件
 gulp.task('minify-css', function () {
     return gulp.src('./public/**/*.css')
-        .pipe(minifycss())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./public'));
 });
+
 
 // 压缩html文件
 gulp.task('minify-html', function () {
